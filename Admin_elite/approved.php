cef8ab9aa2dcd62cd2fb2c51d2ab1b11003c72cf
Keerthi-U <keerthiiachu173@gmail.com>
2022-04-09 17:49:57 +0530

    @changes

--- Admin_elite/approved.php
+++ Admin_elite/approved.php
@@ -336,7 +336,7 @@ $result = mysqli_query($conn,$query);
              <div class="sel" id="<?php echo $row['id'];?>">
              <select class="drop-d" id="statuss" name="statuss" value="<?php echo $row['approved_status'];?>">
              <option value="<?php echo $row['approved_status'];?>"><?php  $row['approved_status'];
-             if($row['approved_status'] == '' or '1'){
+             if($row['approved_status'] == '1'){
                  echo "pending";
              }elseif($row['approved_status'] =='2'){
                 echo "approved";
